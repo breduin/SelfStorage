50e251871e415e3128db25779ee2cd9b45c67592
Fiskless <Fiskless@gmail.com>
2021-11-18 19:39:14 +0500

    Correct phonefield in warehouse

--- apps/storage/models.py
+++ apps/storage/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from .validators import lat_validators, lng_validators
 from django.conf import settings
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Warehouse(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название склада')
     description = models.TextField(verbose_name='Подробное описание склада')
     address = models.CharField(max_length=200, unique=True, verbose_name='Адрес склада')
-    phone = models.PositiveSmallIntegerField(verbose_name='Телефонный номер склада')
+    phone = PhoneNumberField(verbose_name='Телефонный номер склада',
+                             help_text='Введите номер телефона, например, +79999999999')
     latitude = models.FloatField(validators=lat_validators, verbose_name='Широта', )
     longitude = models.FloatField(validators=lng_validators, verbose_name='Долгота')
     slug = models.SlugField(max_length=100, unique=True)

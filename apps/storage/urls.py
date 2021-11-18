5d7f71c10b06497fc36004c13bbbbd92ad37f8b5
Oleg Tarasov <tolegu@ya.ru>
2021-11-18 14:35:09 +0300

    главная страница: карта, картинки, ссылки

--- apps/storage/urls.py
+++ apps/storage/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import map_view, calculator, user_orders
+from .views import main_page, calculator, user_orders
 
 urlpatterns = [
-    path('', map_view, name='main_page'),
     path('storage/<int:category_id>', calculator, name='calculator'),
-    path('user-orders/<int:user_id>', user_orders, name='user_orders')
+    path('user-orders/<int:user_id>', user_orders, name='user_orders'),
+    path('', main_page, name='main_page'),
 ]
--- apps/storage/views.py
+++ apps/storage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.conf import settings
 
-def map_view(request):
+def main_page(request):
     warehouses = {
         'Склад №1': [[55.705546, 37.479194], 'Краткое описание №1'],
         'Склад №2': [[55.654628, 37.530185], 'Краткое описание №2'],
@@ -9,7 +10,11 @@ def map_view(request):
         'Склад №4': [[55.827428, 37.622504], 'Краткое описание №4'],
     }
 
-    return render(request, 'map.html', context={'warehouses': warehouses})
+    return render(request, 'main_page.html', context={
+                                        'warehouses': warehouses,
+                                        'YANDEX_MAPS_API_KEY': settings.YANDEX_MAPS_API_KEY,
+                                        }
+                 )
 
 
 def calculator(request, category_id):

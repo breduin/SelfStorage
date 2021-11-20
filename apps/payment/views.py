70f99759a9b3d1206bd580164a6e82975bfbcff9
Fiskless <Fiskless@gmail.com>
2021-11-20 16:21:52 +0500

    Correct payment

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,14 +1,16 @@
 import stripe
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.conf import settings
-from apps.storage.models import Order
+from apps.storage.models import Order, OrderUnit
+from django.db.models import Avg, Count, Min, Sum
 
 
 def make_payment(request, order_id):
     stripe.api_key = settings.STRIPE_PUBLISHABLE_KEY
 
-    order = Order.objects.get(id=order_id)
+    order = Order.objects.annotate(order_price=Sum('rent_order__price')).get(id=order_id)
+    order_unit = OrderUnit.objects.filter(order=order)
 
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
@@ -16,22 +18,25 @@ def make_payment(request, order_id):
             'price_data': {
                 'currency': 'rub',
                 'product_data': {
-                    'name': order.id,
+                    'name': f'Ваш заказ № {order_id}',
                 },
-                'unit_amount': order.get_sum()*100,
+                'unit_amount': int(order.order_price)*100,
             },
             'quantity': 1,
         }],
         mode='payment',
-        success_url='http://localhost:8000/payment/success',
-        cancel_url='http://localhost:8000/',
+        success_url=f'http://localhost:8000/order/{order_id}/payment/success/',
+        cancel_url=f'http://localhost:8000/order/{order_id}/',
     )
 
     return redirect(session.url, code=303)
 
 
-class SuccessView(TemplateView):
-    template_name = "success.html"
+def pay_success(request, order_id):
+    order = Order.objects.get(id=order_id)
+    order.status = 'ORDER'
+    order.save()
+    return render(request, "success.html")
 
 
 class CancelledView(TemplateView):

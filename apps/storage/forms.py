915f3fed9769fbff314adcfa0703e888e6282ddc
Oleg Tarasov <tolegu@ya.ru>
2021-11-22 17:42:45 +0300

    стилистические правки

--- apps/storage/forms.py
+++ apps/storage/forms.py
@@ -49,7 +49,6 @@ class OrderUnitForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         category_id = kwargs.pop('category_id', None)
         super(OrderUnitForm, self).__init__(*args, **kwargs)
-        # self.fields['unit'].queryset = Unit.objects.filter(category__id=category_id).order_by('name')
 
 
 class OrderForm(forms.ModelForm):
